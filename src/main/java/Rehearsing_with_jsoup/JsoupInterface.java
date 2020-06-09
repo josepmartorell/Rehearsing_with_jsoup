59e69291aaf521265f262f687ddfbff223571ddb
josepmartorell <martorelljosep>
2020-06-09 10:49:55 +0200

    upload new package structure

---
+++ src/main/java/Rehearsing_with_jsoup/JsoupInterface.java
@@ -0,0 +1,42 @@
+package Rehearsing_with_jsoup;
+
+/**
+ *
+ * @author jmartorell
+ */
+public class JsoupInterface {
+
+    static final short ID_NAME = 0;
+    static final short ID_PUNCTUATION = 1;
+    /* From a String array that contains the different menu options, display this properly formatted.
+     * The first position in that array is the title of the menu, not an option.
+     */
+    public static void showMenu(String[] menu){
+        System.out.print("--------------------------------------------------------------------------\n" +
+                         "* * * * * *    JTECH web scraping panel (www.jtech.network)    * * * * * *\n" +
+                         "--------------------------------------------------------------------------\n");
+
+        for (int i = 0; i < menu.length; i++){
+            System.out.print(menu[i] + "\n");
+        }
+
+    }
+    /* From an array containing a ranking of names and punctuation (1st and 2nd column), display it on the screen
+     * properly formatted, with the columns well aligned regardless of the length of the characters.
+     */
+
+    public static void showRanking(String[][]ranking){
+        System.out.print("------------------------------------------\n" +
+                         "TARGET                          COLLATIONS\n" +
+                         "------------------------------------------\n");
+        int i = 0;
+        while ( i < ranking.length){
+            System.out.print(String.format("%-18s", ranking[i][ID_NAME]) + "\t\t");
+            System.out.print(String.format("%18s",ranking[i][ID_PUNCTUATION ]) + "\n");
+            i++;
+        }
+
+    }
+
+
+}
---
+++ src/main/java/Rehearsing_with_jsoup/JsoupUtils.java
@@ -0,0 +1,58 @@
+package Rehearsing_with_jsoup;
+
+import java.util.Scanner;
+/**
+ *
+ * @author jmartorell
+ */
+public class JsoupUtils {
+
+    /* Used in any main program, it allows you to capture an integer by keyboard after displaying the
+     * message corresponding to the first parameter. In case of input error (this is not an integer)
+     * displays the error message (second parameter) and asks for the integer again until the entry is correct.
+     */
+    public static int askEnter(String message, String errorMessage) {
+
+        Scanner scan = new Scanner(System.in);
+        boolean givenCorrect = true;
+        int userOption = 0;
+        System.out.println(message);
+        givenCorrect = scan.hasNextInt();
+        do{
+            givenCorrect = scan.hasNextInt();
+            if(!givenCorrect){
+                System.out.println(errorMessage);
+            }else{
+                userOption = scan.nextInt();
+            }
+            scan.nextLine();
+        }while(!givenCorrect);
+
+        return userOption;
+    };
+    /* With the same behavior as above but with the String data type, it asks for a text string
+     * having previously displayed the message corresponding to the first parameter and, in case it is empty,
+     * insists on displaying the error message corresponding to the second parameter, until the entry is correct.
+     */
+    public static String askString(String missatge, String missatgeError){
+
+        Scanner scan = new Scanner(System.in);
+        boolean givenCorrect = true;
+        String targetName = "";
+        System.out.println(missatge);
+        givenCorrect= scan.hasNextLine();
+        do{
+            //givenCorrect = scan.hasNextLine();
+            if(!givenCorrect){
+                System.out.println(missatgeError);
+            }else{
+                targetName = scan.nextLine();
+            }
+            //scan.nextLine();
+        }while(!givenCorrect);
+
+        return targetName;
+    };
+
+
+}
